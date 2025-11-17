4f5f80124fff71277cb7063b683588ad272e14a7
Dennisharas <densonharas@gmail.com>
2025-11-17 19:04:44 +0100

    iowe

--- interaction-1.js
+++ interaction-1.js
@@ -118,8 +118,8 @@ function playAudio() {
     // them printed on the console of your browser when you load the page)
     // For example if you change to a bell sound, here you could use "/churchBell/gate" instead of
     // "/thunder/rumble".
-    dspNode.setParamValue("/bubble", 1)
-    setTimeout(() => { dspNode.setParamValue("/bubbles", 0) }, 100);
+    dspNode.setParamValue("/bubble/gate", 1)
+    setTimeout(() => { dspNode.setParamValue("/bubble/gate", 0) }, 100);
 }
 
 //==========================================================================================
